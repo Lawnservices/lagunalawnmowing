116c71a80f73b3292189fb3b92b10095797e8ddb
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-07-23 09:46:24 -0400

    fix

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const fecha = new Date();
+    document.getElementById('dia').textContent = `Copyright LagunaLawnServices ${fecha.getFullYear()}`;
+  });
+ 
