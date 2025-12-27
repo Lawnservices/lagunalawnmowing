ab4f8598dc425b9b29d618b4c0b36fdbb7a501e3
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-12-27 14:50:02 -0500

    fix

---
+++ script/script.js
@@ -0,0 +1,4 @@
+let fecha = new Date();
+let logo = '©';
+let texto = 'Miguel Laguna Lawn Services. All Rights Reserved';
+document.getElementById('footer').textContent = `${logo} ${fecha.getFullYear()} ${texto}`;
