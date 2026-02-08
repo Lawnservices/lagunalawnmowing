c6daf90e7046ffb45426a77ca42fc278bac46f82
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-02-08 14:06:55 -0500

    fix

--- images.js
+++ images.js
@@ -11,3 +11,15 @@ fetch("https://mlaguna.pythonanywhere.com/api/images")
   })
   .catch(err => console.error(err));
  
+
+ 
+let visitado = document.getElementById('guarda');
+let visitas = localStorage.getItem('visitas');
+
+if (!visitas) {
+  localStorage.setItem('visitas', '1');
+  visitado.textContent = "THE BEST PRICES IN THE AREA 👋";
+} else {
+  visitado.textContent = "Thanks for coming back 🌱";
+  visitado.style.color = 'blue';
+}
